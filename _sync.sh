2dcf376d7f86412fb36b4ab71dee62c2de0ab8b9
Travis Briggs <audiodude@gmail.com>
2016-09-23 22:18:57 +0000

    Modify sync script

--- _sync.sh
+++ _sync.sh
@@ -1,2 +1,2 @@
 # use `pyenv activate s3cmd` before running this script.
-s3cmd put --recursive --exclude '.git/*' --exclude '_*' . s3://mazega.me/
+s3cmd put --guess-mime-type --no-mime-magic --recursive --exclude '.git/*' --exclude '_*' --exclude '.python-version' . s3://mazega.me/
